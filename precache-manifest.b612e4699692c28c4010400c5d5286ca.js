e557385b4d7f79e7fe3c630228e03a2c7adbbdbf
Yannik <y.vole@werbeagentur-gottschalg.de>
2018-10-12 12:10:11 +0200

    Updates

---
+++ precache-manifest.b612e4699692c28c4010400c5d5286ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "f75b49eab77821edad6b",
+    "url": "/build/static/js/main.f75b49ea.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/build/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f75b49eab77821edad6b",
+    "url": "/build/static/css/main.1bc2fd09.chunk.css"
+  },
+  {
+    "revision": "baa006efdbf859797b9ed38c03d7787f",
+    "url": "/build/index.html"
+  }
+];
